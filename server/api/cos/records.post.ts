d1880e6e2e6fe34d0d9886cc4d2b65d54a13b74d
AceXiamo <xm17906193@163.com>
2023-09-18 01:03:24 +0800

    🚀 feat: api modify

---
+++ server/api/cos/records.post.ts
@@ -0,0 +1,86 @@
+import COS, { COSOptions, GetBucketParams, GetBucketResult } from 'cos-nodejs-sdk-v5'
+import dayjs from './../../../node_modules/dayjs/esm/index';
+
+const options: COSOptions = {
+  SecretId: '',
+  SecretKey: '',
+}
+
+const cos = new COS(options)
+const getFiles = (next?: string): Promise<GetBucketResult> => {
+  return new Promise((resolve, reject) => {
+    let opts: GetBucketParams = {
+      Bucket: '',
+      Region: '',
+      Prefix: 'wq/live/',
+      Delimiter: '/',
+      MaxKeys: 10,
+    }
+    if (next) opts.Marker = next
+    cos.getBucket(opts, function (err, data) {
+      resolve(data)
+    })
+  })
+}
+
+let fileNums: number | null = null
+const getAllFileNum = (): Promise<number> => {
+  return new Promise((resolve, reject) => {
+    if (fileNums) resolve(fileNums)
+    else {
+      cos.getBucket(
+        {
+          Bucket: 'xiamo-1256935307',
+          Region: 'ap-hongkong',
+          Prefix: 'wq/live/',
+          Delimiter: '/',
+          MaxKeys: 1000,
+        },
+        function (err, data) {
+          fileNums = data.Contents?.length
+          resolve(fileNums)
+        }
+      )
+    }
+  })
+}
+
+export type RecordRes = {
+  size?: number
+  next?: string
+  items?: Record[]
+}
+
+export type Record = {
+  key?: string
+  size?: number
+  begin?: string
+  title?: string
+}
+
+const regex = /\[(.*?)\]/g
+export default defineEventHandler(async (event) => {
+  const query: { next: string } = getQuery(event)
+  const cosr = await getFiles(query.next)
+  let result = {
+    size: await getAllFileNum(),
+    next: cosr.NextMarker,
+    items: cosr.Contents.map((v) => {
+      return {
+        key: v.Key,
+        size: Number(v.Size),
+        begin: '-',
+        title: '-',
+      }
+    }),
+  }
+  result.items.forEach((item) => {
+    const match = item.key?.match(regex)
+    if (match) {
+      const begin = match[0].replace(/\[|\]/g, '')
+      item.begin = begin.replace(/_/g, ':')
+      item.title = match[1].replace(/\[|\]/g, '')
+    }
+  })
+  return result
+})
