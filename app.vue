dd2503cfd6431a9002fc73be84d1b5e48d88cbfc
AceXiamo <xiamo@qwq.link>
2023-09-19 21:01:35 +0800

    chore: format

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
 <template>
   <RouterView></RouterView>
 </template>
+
+<style>
+@font-face {
+  font-family: "AllFont";
+  src: url("https://alioss.xiamoqwq.com/font/web_font.woff2");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "emoji-font";
+  src: url("https://alioss.xiamoqwq.com/font/seguiemj.ttf");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+*:not([class*="icon"]):not(i) {
+  font-family: "AllFont", "emoji-font";
+}
+</style>
