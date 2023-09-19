c75f5854f89400ffd6e56e537d5b46512586b466
AceXiamo <xiamo@qwq.link>
2023-09-19 21:01:22 +0800

    🍄 feat: avatar component

---
+++ pages/index/components/WQAvatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :class="[
+      `bg-cover relative`,
+      `before:content-[''] before:absolute before:top-0 before:scale-[1.6] before:bottom-0 before:bg-cover`,
+      `before:left-0 before:right-0 before:bg-[url('~/assets/images/avatar-container.webp')]`,
+    ]"
+    :style="{
+      width: `${props.size}px`,
+      height: `${props.size}px`,
+    }"
+  >
+    <img
+      src="~/assets/images/avatar.avif"
+      class="w-full h-full object-cover rounded-full"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  size?: number
+}>(), {
+  size: 60,
+})
+</script>
+
