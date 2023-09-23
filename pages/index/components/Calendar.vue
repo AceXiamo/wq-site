deac1717d605fbbe52d280b17f82b1009010648d
AceXiamo <xiamo@qwq.link>
2023-09-23 16:52:47 +0800

    feat: index page saved

---
+++ pages/index/components/Calendar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col w-full">
+    <div class="flex items-end gap-[10px]">
+      <div class="flex items-center gap-[10px]">
+        <span>🗓️</span>
+        <UBadge color="white" variant="solid" size="md">
+          {{ currentY }}
+        </UBadge>
+        <UBadge color="white" variant="solid" size="md">
+          {{ currentM }}
+        </UBadge>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import dayjs from "dayjs";
+
+const now = dayjs();
+const currentY = ref(now.format("YYYY"));
+const currentM = ref(now.format("MM"));
+</script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,56 +2,16 @@
   <div class="flex flex-col p-[20px] gap-[20px] min-h-screen">
     <div class="flex gap-[20px] items-center">
       <WQAvatar />
+      <span class="ml-[20px] text-[30px]">Neko's Record Site</span>
       <div class="ml-auto">
         <ColorMode />
       </div>
     </div>
-    <div class="flex gap-[20px] flex-auto">
-      <div class="w-[240px]">
-        <UCard>
-          <Nav :liveRecCount="table?.data.size" />
-        </UCard>
-        <Disk />
-      </div>
-      <!-- <Table class="flex-auto" ref="table" /> -->
-      <div
-        class="flex-auto flex flex-col items-center"
-      >
-        <div class="flex-auto h-0 overflow-auto flex flex-col gap-[20px] bg-red">
-          <div class="flex gap-[50px]" v-for="index of 5" :key="index">
-            <WQAvatar />
-            <div
-              class="w-[600px] bg-slate-100 dark:bg-slate-800 rounded-md p-[20px]"
-            >
-              <div class="">
-                <span class="text-[13px]">
-                  【重大通知】
-                  距离林晚秋小姐的生日会还有不到一个月的时间了，时间过得好快呀，好想和呆秋卟一起过生日~以下是相关事项通知：<br />
-                  ❤关于实体礼物邮寄❤ <br />
-                  如果有想邮寄实体礼物的，可以邮寄到代收点：<br />
-                  广东省惠州市龙门县塔新路三巷八号 联系手机:13249362470<br />
-                  请在9.31号前发出，生日会当天也会直播开箱，一起看看收到的大家送的生日礼物~<br />
-                  ❤关于棉花糖收集❤<br />
-                  呆秋卟有想对秋秋说的话都可以写到里面，秋秋都能看到的，也会在生日会当天和大家一起念棉花糖，希望大家可以踊跃投稿！<br />
-                  投稿地址：https://2some.one/@天才猪猪林晚秋wq<br />
-                  还可以扫二维码或者发送到这个邮箱 3493259173170099@2some.one<br />
-                  （提醒：只支持文字投稿，图片是收不到的[眠眠兔_偷看]）
-                </span>
-              </div>
-              <div class="grid grid-cols-3 gap-[20px] mt-[20px]">
-                <div
-                  class="aspect-[1/1] rounded-md overflow-hidden"
-                  v-for="index of 3"
-                >
-                  <img
-                    class="w-full h-full object-cover"
-                    src="https://image.qwq.link/images/2023/08/30/twitter_takesinobu_20230815-010318_1691254202767667200_photo.jpg"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="flex flex-auto h-0 gap-[50px]">
+      <div class="flex-1"></div>
+      <RecordList></RecordList>
+      <div class="flex-1">
+        <Calendar />
       </div>
     </div>
   </div>
@@ -60,12 +20,10 @@
 <script setup lang="ts">
 import WQAvatar from "./components/WQAvatar.vue";
 import ColorMode from "./components/ColorMode.vue";
-import Nav from "./components/Nav.vue";
-import Disk from "./components/Disk.vue";
-import Table from "./components/Table.vue";
+import Calendar from "./components/Calendar.vue";
 import Wave from "@/assets/js/wave";
+import RecordList from "./components/RecordList.vue";
 
-const table = ref();
 onMounted(() => {
   const wave = new Wave("body", {
     number: 2,
@@ -80,4 +38,5 @@ onMounted(() => {
   });
   wave.animate();
 });
+
 </script>
