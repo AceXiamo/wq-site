13b192382a224cba5ae03bd73eb45f0afc1ff1b7
AceXiamo <xiamo@qwq.link>
2023-09-23 16:51:05 +0800

    💣 feat: loading component

---
+++ utils/Loading/index.ts
@@ -0,0 +1,39 @@
+import { h, render } from "vue";
+import Loading from "@/utils/Loading/Loading.vue";
+
+const show = (options: {
+  isFull?: boolean;
+  element?: string;
+}): {
+  close: () => void;
+} => {
+  const div = document.createElement("div");
+  div.style.position = "absolute";
+  div.style.top = "0";
+  div.style.left = "0";
+  div.style.width = "100%";
+  div.style.height = "100%";
+  div.style.zIndex = "9999";
+  const vNode = h(Loading, {
+    close: () => {
+      div.remove();
+    },
+  });
+  console.log(vNode);
+  vNode.appContext = useNuxtApp().vueApp._context;
+  render(vNode, div);
+
+  if (options.element) {
+    const target = document.querySelector(options.element);
+    if (target) {
+      target.appendChild(div);
+    }
+  } else {
+    document.body.appendChild(div);
+  }
+  return {
+    close: vNode.component?.exposed?.close,
+  };
+};
+
+export default { show };
