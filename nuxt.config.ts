a015815200eaa3ee136ad957c5ff30ccdeba6546
AceXiamo <xiamo@qwq.link>
2023-09-25 09:44:09 +0800

    chore: add auto-animate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@formkit/auto-animate/nuxt"],
   devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+  colorMode: {
+    classSuffix: "",
+  },
 });
