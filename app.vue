808ffc62cd98438b61a3b9e690cbc72c68a4a82c
AceXiamo <xiamo@qwq.link>
2023-09-25 09:44:34 +0800

    feat: move css to main.css

--- app.vue
+++ app.vue
@@ -1,33 +1,3 @@
 <template>
   <RouterView></RouterView>
 </template>
-
-<style lang="less">
-@font-face {
-  font-family: "AllFont";
-  // src: url("https://file.qwq.link/font/zzaw.woff2");
-  src: url("https://file.qwq.link/font/InputMono-Regular.ttf");
-  font-weight: 400;
-  font-style: normal;
-  font-display: swap;
-}
-
-@font-face {
-  font-family: "emoji-font";
-  src: url("https://file.qwq.link/font/seguiemj.ttf");
-  font-weight: 400;
-  font-style: normal;
-  font-display: swap;
-}
-
-*:not([class*="icon"]):not(i) {
-  font-family: "AllFont", "emoji-font";
-}
-
-/* hide scroll */
-.hide-scroll {
-  &::-webkit-scrollbar {
-    width: 0;
-  }
-}
-</style>
