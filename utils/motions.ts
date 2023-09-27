955583e77eb5f18e4fddb1bc770eab412a25c277
AceXiamo <xiamo@qwq.link>
2023-09-27 20:08:56 +0800

    feat：motion config

--- utils/motions.ts
+++ utils/motions.ts
@@ -29,3 +29,61 @@ export const leftMontionConfig = (delay: number = 0) => {
     },
   };
 };
+
+export const bottomToTop = (delay: number = 0) => {
+  return {
+    initial: {
+      y: 20,
+      opacity: 0,
+    },
+    enter: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const rightToLeft = (delay: number = 0) => {
+  return {
+    initial: {
+      x: 20,
+      opacity: 0,
+    },
+    enter: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const musicPlay = () => {
+  return {
+    initial: {
+      opacity: 0,
+      x: 20,
+    },
+    enter: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+};
+
+export const musicLine = () => {
+  return {
+    initial: {
+      y: 20,
+      opacity: 0,
+    },
+    enter: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+};
