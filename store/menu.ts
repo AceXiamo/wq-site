703e628e1cb941183b8505acc14f155b9402c948
AceXiamo <xiamo@qwq.link>
2023-09-27 20:08:37 +0800

    feat: menu store

--- store/menu.ts
+++ store/menu.ts
@@ -1,32 +1,64 @@
 import { defineStore } from "pinia";
 
-type Menu = {
+export type Menu = {
   value: string;
   icon: string;
   title: string;
+  url: string;
   delay?: number;
 };
 
-export const useMenuStore = defineStore("menu", () => {
-  const menus = ref<Menu[]>([
-    {
-      value: "record",
-      icon: "i-heroicons-video-camera",
-      title: "录播",
-      delay: 200,
-    },
-    {
-      value: "playlist",
-      icon: "i-heroicons-musical-note-20-solid",
-      title: "歌单",
-      delay: 300,
-    },
-  ]);
+export const useMenuStore = defineStore(
+  "menu",
+  () => {
+    const menus = ref<Menu[]>([
+      {
+        value: "record",
+        icon: "i-heroicons-video-camera",
+        title: "小猫的录播",
+        url: "/",
+        delay: 200,
+      },
+      {
+        value: "playlist",
+        icon: "i-heroicons-musical-note-20-solid",
+        title: "小猫的歌单",
+        url: "/playlist",
+        delay: 300,
+      },
+      {
+        value: "bili",
+        icon: "i-heroicons-sparkles-20-solid",
+        title: "小猫的B站空间",
+        url: "https://space.bilibili.com/2115353085",
+        delay: 400,
+      },
+      {
+        value: "bili",
+        icon: "i-heroicons-film",
+        title: "小猫的直播间",
+        url: "https://live.bilibili.com/25081972",
+        delay: 500,
+      },
+      {
+        value: "more",
+        icon: "i-heroicons-sun-20-solid",
+        title: "More",
+        url: "dev",
+        delay: 600,
+      },
+    ]);
 
-  const active = ref<string>("record");
+    const active = ref<string>("record");
 
-  return {
-    menus,
-    active,
-  };
-});
+    return {
+      menus,
+      active,
+    };
+  },
+  {
+    persist: {
+      key: "menu",
+    },
+  }
+);
