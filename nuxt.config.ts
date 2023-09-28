93b26b1d8413df658f674225d54197d45bf23c4b
AceXiamo <xiamo@qwq.link>
2023-09-28 23:21:28 +0800

    feat: add icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
   colorMode: {
     classSuffix: "",
   },
+  ui: {
+    icons: ['heroicons', 'iconamoon']
+  },
   runtimeConfig: {
     public: {
       motion: {
