12950e18ad485a00cec11c58c60e2e908ef5d176
AceXiamo <xiamo@qwq.link>
2023-10-01 19:46:11 +0800

    chore: remove ts

--- utils/music.ts
+++
@@ -1,5 +0,0 @@
-export const PLAY_STATUS: PlayStatus[] = ["play", "pause"];
-export type PlayStatus = "play" | "pause";
-
-export type MusicStatus = "repeat" | "repeat-one" | "shuffle";
-export const MUSIC_STATUS: MusicStatus[] = ["repeat", "repeat-one", "shuffle"];
