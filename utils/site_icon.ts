9fa7cd36179667ed8d993d1f05b694ddf9f6db4d
AceXiamo <xiamo@qwq.link>
2023-10-13 14:42:33 +0800

    🌻 feat: site icon

--- utils/site_icon.ts
+++ utils/site_icon.ts
@@ -1,20 +1,26 @@
-import Cat_1 from "~/assets/icon/cat_1.jpg";
-import Cat_2 from "~/assets/icon/cat_2.jpg";
-import Cat_3 from "~/assets/icon/cat_3.jpg";
-import Cat_4 from "~/assets/icon/cat_4.jpg";
-import Cat_5 from "~/assets/icon/cat_5.jpg";
-import Cat_6 from "~/assets/icon/cat_6.jpg";
+import Cat_1 from "~/assets/icon/cat_1.png";
+import Cat_2 from "~/assets/icon/cat_2.png";
+import Cat_3 from "~/assets/icon/cat_3.png";
+import Cat_4 from "~/assets/icon/cat_4.png";
+import Cat_5 from "~/assets/icon/cat_5.png";
+import Cat_6 from "~/assets/icon/cat_6.png";
 
 const icons = [Cat_1, Cat_2, Cat_3, Cat_4, Cat_5, Cat_6];
+const iconMap = new Map<number, string>();
 
 const getBase64 = (index: number): Promise<string> => {
   return new Promise((resolve) => {
+    if (iconMap.has(index)) {
+      resolve(iconMap.get(index)!);
+      return;
+    }
     fetch(icons[index])
       .then(response => response.blob())
       .then(blob => {
         const reader = new FileReader();
         reader.onloadend = () => {
           const base64Data = reader.result;
+          iconMap.set(index, String(base64Data));
           resolve(String(base64Data));
         };
         reader.readAsDataURL(blob);
@@ -43,7 +49,7 @@ const init = () => {
       link.href = base64;
       document.getElementsByTagName("head")[0].appendChild(link);
     });
-  }, 300)
+  }, 240)
 };
 
 export default { init };
