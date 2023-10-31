d3aef2ee3f03a6f51cf0d68cc48d2d6cb4716cff
AceXiamo <xiamo@qwq.link>
2023-10-31 11:39:37 +0800

    🔗 chore: @media config of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
-    "./components/**/*.vue",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./framework/**/*.{vue,ts}",
-    "./store/**/*.ts",
+    './components/**/*.vue',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './framework/**/*.{vue,ts}',
+    './store/**/*.ts',
   ],
+  theme: {
+    screens: {
+      '2xl': { max: '1535px' },
+      'xl': { max: '1279px' },
+      'lg': { max: '1120px' },
+      'md': { max: '767px' },
+      'sm': { max: '639px' },
+    },
+  },
 };
