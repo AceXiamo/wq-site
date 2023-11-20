500e196a8c63d2b4c0264a55d652311e9e894907
AceXiamo <xiamo@qwq.link>
2023-11-20 17:21:56 +0800

    refactor: add desc

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,14 +1,6 @@
 <template>
-  <div
-    class="gap-[10px] h-[50px] px-[30px] flex items-end"
-    v-motion="rightToLeft()"
-  >
-    <div
-      :class="[
-        `w-[25px] h-[25px] rounded-md bg-emerald-500 grid place-content-center`,
-        `dark:bg-blue-700`,
-      ]"
-    >
+  <div class="gap-[10px] h-[50px] px-[30px] flex items-end" v-motion="rightToLeft()">
+    <div :class="[`w-[25px] h-[25px] rounded-md bg-emerald-500 grid place-content-center`, `dark:bg-blue-700`]">
       <i class="i-heroicons-rectangle-group text-[14px] text-white" />
     </div>
     <div
@@ -17,9 +9,7 @@
         `dark:bg-blue-700 dark:bg-opacity-30`,
       ]"
     >
-      <span class="text-[12px] text-emerald-500 dark:text-blue-700">{{
-        year
-      }}</span>
+      <span class="text-[12px] text-emerald-500 dark:text-blue-700">{{ year }}</span>
     </div>
     <div
       :class="[
@@ -27,17 +17,13 @@
         `dark:bg-blue-700 dark:bg-opacity-30`,
       ]"
     >
-      <span class="text-[12px] text-emerald-500 dark:text-blue-700">{{
-        month
-      }}</span>
+      <span class="text-[12px] text-emerald-500 dark:text-blue-700">{{ month }}</span>
     </div>
 
     <div
       :class="[
         `w-[25px] h-[25px] rounded-md bg-emerald-500 dark:bg-blue-700 grid place-content-center`,
-        year === lower[0] && month === lower[1]
-          ? `grayscale-[1] cursor-no-drop`
-          : `hover:bg-opacity-70 cursor-pointer`,
+        year === lower[0] && month === lower[1] ? `grayscale-[1] cursor-no-drop` : `hover:bg-opacity-70 cursor-pointer`,
       ]"
       @click="clickHandle(-1)"
     >
@@ -47,38 +33,44 @@
     <div
       :class="[
         `w-[25px] h-[25px] rounded-md bg-emerald-500 dark:bg-blue-700 grid place-content-center`,
-        year === upper[0] && month === upper[1]
-          ? `grayscale-[1] cursor-no-drop`
-          : `hover:bg-opacity-70 cursor-pointer`,
+        year === upper[0] && month === upper[1] ? `grayscale-[1] cursor-no-drop` : `hover:bg-opacity-70 cursor-pointer`,
       ]"
       @click="clickHandle(1)"
     >
       <i class="i-heroicons-chevron-right-20-solid text-[14px] text-white" />
     </div>
+
+    <div class="text-[12px] text-gray-300 dark:text-gray-600">( 仅保存近两月 )</div>
   </div>
   <RecordList ref="recordList"></RecordList>
   <div class="h-[30px]"></div>
 </template>
 
 <script setup lang="ts">
-import { setTitle } from "~/utils/common";
-import dayjs from "dayjs";
+import { setTitle } from '~/utils/common';
+import dayjs from 'dayjs';
 
 const recordList = ref();
 const now = dayjs(),
-  lower = ["2023", "09"],
-  upper = [now.format("YYYY"), now.format("MM")];
-const year = ref<string>(now.format("YYYY"));
-const month = ref<string>(now.format("MM"));
+  lower = [now.format('YYYY'), now.subtract(1, 'month').format('MM')],
+  upper = [now.format('YYYY'), now.format('MM')];
+const year = ref<string>(now.format('YYYY'));
+const month = ref<string>(now.format('MM'));
 
 const clickHandle = (num: number) => {
-  const date = dayjs(`${year.value}-${month.value}`).add(num, "month");
-  year.value = date.format("YYYY");
-  month.value = date.format("MM");
-  recordList.value?.loadData(year.value + "-" + month.value);
+  if (num === 1) {
+    if (year.value === upper[0] && month.value === upper[1]) return;
+  } else if (num === -1) {
+    if (year.value === lower[0] && month.value === lower[1]) return;
+  }
+
+  const date = dayjs(`${year.value}-${month.value}`).add(num, 'month');
+  year.value = date.format('YYYY');
+  month.value = date.format('MM');
+  recordList.value?.loadData(year.value + '-' + month.value);
 };
 
 onMounted(() => {
-  setTitle("✨ 小猫 の 录播");
+  setTitle('✨ 小猫 の 录播');
 });
 </script>
